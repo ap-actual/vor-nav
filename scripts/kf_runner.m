342dc8d31737b5950cb08007b34db11df57d7dbb
TheBlickster <84302883+TheBlickster@users.noreply.github.com>
2026-04-19 18:23:33 -0400

    Update kf_runner.m

--- scripts/kf_runner.m
+++ scripts/kf_runner.m
@@ -20,7 +20,7 @@ VOR_1_SIGMA = 2; % 2-deg 1-sigma
 % clear everything except flight data (in case previous step has been skipped)
 
 % create IMU measurement 
-[imuMeasured, imuTruth] = createImuMeasurements(flightData, 'icao_abe79d_F1');
+[trajImuData, imuTruth] = createImuMeasurements(flightData, 'icao_abe79d_F1');
 
 % load navaid data
 navAids = VorNav.loadVorData('../dataSets/NAVAID_System.csv');
@@ -29,9 +29,22 @@ navAids = VorNav.loadVorData('../dataSets/NAVAID_System.csv');
 vorMeasRate = 1; % hz
 vorMeasLast = 0;
 
-nTimes = numel(imuMeasured.time);
+nTimes = numel(trajImuData.time);
 
 % Initial Conditions
+% x0
+x0 = [0; 0; 0; 0; 0; 0; 0; 0; 0;... % dynamic states, X0 will be set depending on trajectory
+    0.122; 0.122; 0.122; ...        % Accel Sensitivies, based on document
+    4.375; 4.375; 4.375;...         % Gyro Sensitivies, based on document
+    0; 0; 0; ...                    % Zero G Accel
+    0; 0; 0];                       % Zero G Gyro
+
+% P0
+P0 = diag([1e6; 1e6; 1e6; 100; 100; 100; 180; 180; 180;... % Dynamics prior free(?)
+    6.4000e-07; 6.4000e-07; 6.4000e-07;...                 % Accel Sensitivites
+    8.5069e-04; 8.5069e-04; 8.5069e-04;...                 % Gyro Sensitivites
+    469.4444; 469.4444; 469.4444;...                       % Zero G Accel
+    1; 1; 1]);                                             % Zero G Gyro
 
 x = zeros(21,nTimes);
 P = zeros(21,21,nTimes);
@@ -40,7 +53,7 @@ x(:,1) = x0;
 P(:,:,1) = P0;
 
 % run for-loop
-for i = 1:numel(imuMeasured.time) 
+for i = 1:numel(trajImuData.time) 
 
     % -- KF PROPAGATION STEP HERE --
     x(:,i+1) = dynamics(x(:,i), a, td,dt,i);
@@ -53,10 +66,10 @@ for i = 1:numel(imuMeasured.time)
 
 
     % if VOR meas happens, run VOR meas
-    if imuMeasured.time(i) - vorMeasLast > 1/vorMeasRate
+    if trajImuData.time(i) - vorMeasLast > 1/vorMeasRate
         
         % generate VOR measurement
-        truthLla = [imuMeasured.lat(i), imuMeasured.lon(i), imuMeasured.alt(i)];
+        truthLla = [trajImuData.lat(i), trajImuData.lon(i), trajImuData.alt(i)];
 
         % create vor meas
         vorMeasData = VorNav.vorMeas(truthLla, "lla", navAids);
@@ -75,7 +88,7 @@ for i = 1:numel(imuMeasured.time)
 
 
         % reset last meas time
-        vorMeasLast = imuMeasured.time(i);
+        vorMeasLast = trajImuData.time(i);
     end
 
 end
