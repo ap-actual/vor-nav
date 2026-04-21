c2243e6fc997698949e4f2410d347126be83b168
TheBlickster <84302883+TheBlickster@users.noreply.github.com>
2026-04-20 22:01:01 -0400

    Update kf_runner.m

--- scripts/kf_runner.m
+++ scripts/kf_runner.m
@@ -42,11 +42,11 @@ lla0 = [trajImuData.lat(1), trajImuData.lon(1), trajImuData.alt(1)];
 initialUTC = datetime(2000, 1, 1, 0, 0, 0, 0, 'TimeZone', 'UTC', 'Format', 'dd-MMM-uuuu HH:mm:ss.SSS');
 pos0 = lla2eci(lla0, datevec(initialUTC))';
 pos0ECEF = lla2ecef(lla0);
-velNED0 = trajImuData.velocityNED(1,:);
+velNED0 = trajImuData.velocityNED(1,:) .* 0.514444;
 [velECEFX0, velECEFY0, velECEFZ0] = ned2ecefv(velNED0(1), velNED0(2), velNED0(3), lla0(1), lla0(2));
 velECEF0 = [velECEFX0, velECEFY0, velECEFZ0];
 [~, vel0] = ecef2eci(initialUTC, pos0ECEF, velECEF0);
-orn0 = trajImuData.eulerAngles(1,:)';
+orn0 = rad2deg(trajImuData.eulerAngles(1,:)');
 x0 = [pos0; vel0; orn0;... % dynamic states, X0 will be set depending on trajectory
     0.122; 0.122; 0.122; ...        % Accel Sensitivies, based on document
     4.375; 4.375; 4.375;...         % Gyro Sensitivies, based on document
@@ -75,14 +75,17 @@ for i = 1:numel(trajImuData.time)
 
    currentUTC = initialUTC + seconds(dt * i);
    tB2ECI = calculateBody2ECI(x(:,i), currentUTC);
-   sf = trajImuData.specificForceBodyMeas(i,:);
-   bodyRates = trajImuData.gyroRatesMeas(i,:);
+   sf = trajImuData.specificForceBodyTruth(i,:);
+   bodyRates = trajImuData.gyroRatesTruth(i,:);
 
     % -- KF PROPAGATION --
     x(:,i+1) = dynamics(x(:,i), sf, bodyRates, dt, tB2ECI);
     F = jacobian_f(x(:,i), sf, bodyRates, dt, tB2ECI);
     Phi = expm(F * dt);
     P(:,:,i+1) = Phi * P(:,:,i) * Phi' + Q;
+
+    trueLLA = [trajImuData.lat(i), trajImuData.lon(i), trajImuData.alt(i)];
+    trueECI = lla2eci(trueLLA, datevec(currentUTC));
 
     % if VOR meas happens, run VOR meas
     if trajImuData.time(i) - vorMeasLast > 1/vorMeasRate
@@ -110,9 +113,7 @@ for i = 1:numel(trajImuData.time)
         H = predictedBearingJacobian(x(:,i+1), vorMeasData, visibleVorIdents, currentUTC);
 
         % compute HX
-        trueLLA = [trajImuData.lat(i), trajImuData.lon(i), trajImuData.alt(i)];
-        trueECI = lla2eci(trueLLA, datevec(currentUTC));
-        hx = predictedBearings(trueECI, vorMeasData, visibleVorIdents, currentUTC);
+        hx = predictedBearings(trueECI', vorMeasData, visibleVorIdents, currentUTC);
         z_vor = deg2rad(vorMeasBearing');
 
         innovation = mod(z_vor - hx + pi, 2*pi) - pi;  % wraps to (-pi, pi]
@@ -159,7 +160,7 @@ function xnext = dynamics(x,a,td,dt,tB2ECI)
     % Angular rates into orientation; subtracting out our expected bias and
     % scale factor
     % dx(7:9) = (td' - x(19:21))./(1 + x(13:15) .* 1e-3);
-    dx(7:9) = td';
+    dx(7:9) = rad2deg(td');
 
     % Don't propogate bias and scale factor
     dx(10:21) = zeros(12,1);
@@ -193,33 +194,42 @@ end
 % tBody2ECI
 function tB2ECI = calculateBody2ECI(x, UTC)
     % body to NED
-    phi = x(7); sphi = sind(phi); cphi = cosd(phi);
-    theta = x(8); stheta = sind(theta); ctheta = cosd(theta);
-    psi = x(9); spsi = sind(psi); cpsi = cosd(psi);
-    
-    tB2NED = [ctheta * cpsi, ctheta * spsi, -stheta;
-        sphi * stheta * cpsi - cphi * spsi, sphi * stheta * spsi + cphi * cpsi, sphi * ctheta;
-        cphi * stheta * cpsi + sphi * spsi, cphi * stheta * spsi - sphi * cpsi, cphi * ctheta];
+    tB2NED = angle2dcm(deg2rad(x(7)),deg2rad(x(8)),deg2rad(x(9)),'ZYX');
 
-    % NED to ECEF
     lla = eci2lla(x(1:3)', datevec(UTC));
-    lat = lla(1); slat = sind(lat); clat = cosd(lat);
-    lon = lla(2); slon = sind(lon); clon = cosd(lon);
+    tECEF2NED = dcmecef2ned(lla(1), lla(2));
 
-    tNED2ECEF = [-slat * clon, -slat * slon, clat;
-        -slon, clon, 0;
-        -clat * clon, -clat * slon, -slat];
+    tECI2ECEF = dcmeci2ecef('IAU-2000/2006', datevec(UTC));
 
-    % ECEF to ECI
-    omegaE = 7.2921159e-5; % rad/s
-    theta = omegaE * seconds(timeofday(UTC));
-
-    tECEF2ECI = [...
-         cos(theta), -sin(theta), 0;
-         sin(theta),  cos(theta), 0;
-         0,           0,          1];
-
-    tB2ECI = tECEF2ECI * tNED2ECEF * tB2NED;
+    tB2ECI = tECI2ECEF' * tECEF2NED' * tB2NED;
+    % body to NED
+    % phi = x(7); sphi = sind(phi); cphi = cosd(phi);
+    % theta = x(8); stheta = sind(theta); ctheta = cosd(theta);
+    % psi = x(9); spsi = sind(psi); cpsi = cosd(psi);
+    % 
+    % tB2NED = [ctheta * cpsi, ctheta * spsi, -stheta;
+    %     sphi * stheta * cpsi - cphi * spsi, sphi * stheta * spsi + cphi * cpsi, sphi * ctheta;
+    %     cphi * stheta * cpsi + sphi * spsi, cphi * stheta * spsi - sphi * cpsi, cphi * ctheta];
+    % 
+    % % NED to ECEF
+    % lla = eci2lla(x(1:3)', datevec(UTC));
+    % lat = lla(1); slat = sind(lat); clat = cosd(lat);
+    % lon = lla(2); slon = sind(lon); clon = cosd(lon);
+    % 
+    % tNED2ECEF = [-slat * clon, -slat * slon, clat;
+    %     -slon, clon, 0;
+    %     -clat * clon, -clat * slon, -slat];
+    % 
+    % % ECEF to ECI
+    % omegaE = 7.2921159e-5; % rad/s
+    % theta = omegaE * seconds(timeofday(UTC));
+    % 
+    % tECEF2ECI = [...
+    %      cos(theta), -sin(theta), 0;
+    %      sin(theta),  cos(theta), 0;
+    %      0,           0,          1];
+    % 
+    % tB2ECI = tECEF2ECI * tNED2ECEF * tB2NED;
 
     
 end
